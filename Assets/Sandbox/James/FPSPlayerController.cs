5087b7cec65e9b4adf74ae238981f32f26ac6c5e
James Gentry <j.gentry@outlook.com>
2018-02-10 17:58:07 -0600

    added basic FPS camera control

--- Assets/Sandbox/James/FPSPlayerController.cs
+++ Assets/Sandbox/James/FPSPlayerController.cs
@@ -3,6 +3,20 @@ using UnityEngine.Networking;
 
 public class FPSPlayerController : NetworkBehaviour
 {
+	public float PlayerRunSpeed;
+	public float MouseSpeed;
+	public float CameraPitchClamp;
+
+	private float _pitch;
+	private Transform _camera;
+
+	void Start()
+	{
+		_pitch = 0f;
+		_camera = transform.GetChild (0);	
+		_camera.gameObject.GetComponent<Camera> ().cullingMask &= ~(1 << LayerMask.NameToLayer("CameraInvisible"));
+	}
+
 	void Update()
 	{
 		if (!isLocalPlayer)
@@ -10,10 +24,18 @@ public class FPSPlayerController : NetworkBehaviour
 			return;
 		}
 
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * PlayerRunSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * PlayerRunSpeed;
+		var rotation = Input.GetAxis("Mouse X") * MouseSpeed;
+		_pitch -= Input.GetAxis("Mouse Y") * MouseSpeed;
+		_pitch = Mathf.Clamp (_pitch, -CameraPitchClamp, CameraPitchClamp);
+		transform.Translate(x, 0, z);
+		transform.Rotate(0, rotation, 0);
+		_camera.localRotation = Quaternion.AngleAxis (_pitch, Vector3.right);
+	}
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+	void LateUpdate()
+	{
+		
 	}
 }
