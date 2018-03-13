8a21a98d5e176748d13aa303e87337e10ec46cc0
James Gentry <18251756+palpitatertot@users.noreply.github.com>
2018-03-13 11:40:52 -0500

    test pee scene

---
+++ Assets/Sandbox/James/Dog/SPDogController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SPDogController : MonoBehaviour //NetworkBehaviour
+{
+	public float FrontRunSpeed;
+	public float RearRunSpeed;
+	public float FrontTurnSpeed;
+	public float RearTurnSpeed;
+	public float MouseSpeed;
+
+	private Transform _camera;
+	private Transform _frontPivot;
+	private Transform _rearPivot;
+	private Rigidbody _front;
+	private Rigidbody _rear;
+	private float fx, fz, rx, rz, y;
+
+	void Start()
+	{
+		_camera = Camera.allCameras[0].transform;
+		_camera.SetParent(transform);
+		_camera.position = transform.position + new Vector3(0,20,0);
+		_camera.LookAt(transform);
+		_frontPivot = transform.Find("frontPivot");
+		_rearPivot = transform.Find("rearPivot");
+		_front = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+	
+		fx = Input.GetAxis("Horizontal") * FrontTurnSpeed * Time.deltaTime;
+		fz = Input.GetAxis("Vertical") * FrontRunSpeed * Time.deltaTime;
+		rx = Input.GetAxis("Horizontal2") * FrontTurnSpeed * Time.deltaTime;
+		rz = Input.GetAxis("Vertical2") * FrontRunSpeed * Time.deltaTime;
+		//Debug.Log ("fx = " + fx + "fz = " + fz + "rx =" + rx + "rz =" + rz);
+
+	}
+
+	void FixedUpdate(){
+		_front.AddForceAtPosition(transform.forward * fz, _frontPivot.position, ForceMode.Impulse);
+		_front.AddForceAtPosition(transform.right * fx, _frontPivot.position, ForceMode.Impulse);
+		_front.AddForceAtPosition(transform.forward * rz, _rearPivot.position, ForceMode.Impulse);
+		_front.AddForceAtPosition(transform.right * rx, _rearPivot.position, ForceMode.Impulse);
+	}
+
+	void LateUpdate()
+	{
+		_camera.position = transform.position + new Vector3(0,20,0);
+	}
+}
