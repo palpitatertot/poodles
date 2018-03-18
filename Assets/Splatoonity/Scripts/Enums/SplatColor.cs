f037a45fa24d044f2486095d9b07142131204049
CollieSaloon <vcv5733@louisiana.edu>
2018-03-18 17:25:40 -0500

    removing build from repo and adding splat color 'enum'

--- Assets/Splatoonity/Scripts/Enums/SplatColor.cs
+++ Assets/Splatoonity/Scripts/Enums/SplatColor.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SplatColor {
-
-    public enum Color {RED, GREEN, BLUE, CLEAR }
-
+public static class SplatColor
+{
+    public static Vector4 YELLOW = new Vector4(1, 0, 0, 0);
+    public static Vector4 RED = new Vector4(0, 1, 0, 0);
+    public static Vector4 GREEN = new Vector4(0, 0, 1, 0);
+    public static Vector4 BLUE = new Vector4(0, 0, 0, 1);
+    public static Vector4 HUMAN = new Vector4(0, 0, 0, 0);
 }
