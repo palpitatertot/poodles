5b45a441e238a13da07f344f0446adeb55b125e8
Faulk David J <djf8530@louisiana.edu>
2018-03-24 16:10:14 -0500

    Adding Pause Menu made w/ canvas

---
+++ Assets/Sandbox/Dave/pauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour {
+
+	public static bool isPaused = false;
+	public Texture2D menu;
+
+	public GameObject pauseMenuUI;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (isPaused)
+				Resume ();
+			else
+				Pause();
+		}
+	}
+
+	void Resume()
+	{
+		pauseMenuUI.SetActive (false);
+		isPaused = false;
+	}
+
+	void Pause()
+	{
+		pauseMenuUI.SetActive (true);
+		isPaused = true;
+	}
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene ("Lobby");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("Quitting Game");
+		Application.Quit ();
+	}
+
+
+	void OnGui()
+	{
+		GUI.DrawTexture (new Rect (20, 20, menu.width, menu.height), menu);
+	}
+}
--- Assets/Splatoonity/Scripts/Menu.cs
+++ Assets/Splatoonity/Scripts/Menu.cs
@@ -9,18 +9,24 @@ public class Menu : MonoBehaviour {
 	public Texture2D sliderGreen;
 	public Texture2D sliderBlue;
 
+	bool showMenu = false;
+
 	// Use this for initialization
 	//void Start () {
 	
 	//}
 	
 	// Update is called once per frame
-	//void Update () {
-	
-	//}
+	void Update () {
+		if (Input.GetKey (KeyCode.Tab))
+			showMenu = true;
+		else if(showMenu) {
+			showMenu = false;
+		}
+	}
 
 	void OnGUI () {
-		GUI.DrawTexture (new Rect (20, 20, menu.width, menu.height), menu);
+		if(pauseMenu.isPaused) GUI.DrawTexture (new Rect (20, 20, menu.width, menu.height), menu);
 
 		Vector4 scores = SplatManagerSystem.instance.scores + new Vector4(0.1f,0.1f,0.1f,0.1f);
 		float totalScores = scores.x + scores.y + scores.z + scores.w;
@@ -29,10 +35,11 @@ public class Menu : MonoBehaviour {
 		int greenScore = (int)( 512 * ( scores.z / totalScores ) );
 		int blueScore = (int)( 512 * ( scores.w / totalScores ) );
 
-		GUI.DrawTexture (new Rect (20 + menu.width + 20, 20, yelowScore, 30), sliderYellow);
-		GUI.DrawTexture (new Rect (20 + menu.width + 20, 60, redScore, 30), sliderRed);
-		GUI.DrawTexture (new Rect (20 + menu.width + 20, 100, greenScore, 30), sliderGreen);
-		GUI.DrawTexture (new Rect (20 + menu.width + 20, 140, blueScore, 30), sliderBlue);
-
+		if (showMenu) {
+			GUI.DrawTexture (new Rect (20 + menu.width + 20, 20, yelowScore, 30), sliderYellow);
+			GUI.DrawTexture (new Rect (20 + menu.width + 20, 60, redScore, 30), sliderRed);
+			GUI.DrawTexture (new Rect (20 + menu.width + 20, 100, greenScore, 30), sliderGreen);
+			GUI.DrawTexture (new Rect (20 + menu.width + 20, 140, blueScore, 30), sliderBlue);
+		}
 	}
 }
