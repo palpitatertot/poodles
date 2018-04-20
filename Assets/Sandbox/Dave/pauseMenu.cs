d31be1443c47eb775ba28d9361f3f9584c8557a4
Faulk David J <djf8530@louisiana.edu>
2018-04-20 16:43:02 -0500

    Adjusted GUI for score to build off of each other. Adds status for determining man's victory condition. Need to adjust numbers with testing.

--- Assets/Sandbox/Dave/pauseMenu.cs
+++ Assets/Sandbox/Dave/pauseMenu.cs
@@ -8,6 +8,7 @@ using UnityEngine.Networking;
 public class pauseMenu : NetworkBehaviour
 {
 	public Text winnerText;
+	public Text MessText;
 
     public static bool isPaused = false;
     public bool showScore = false;
@@ -22,13 +23,18 @@ public class pauseMenu : NetworkBehaviour
     public Image p2Score;
     public Image p3Score;
 
+	int numPlayers;
+
     public Image waterLevel;
 
     void Start()
     {
         pauseMenuUI = transform.GetChild(0).gameObject;
+		pauseMenuUI.SetActive (false);
         scoreMenuUI = transform.GetChild(1).gameObject;
+		scoreMenuUI.SetActive (false);
         waterLevelUI = transform.GetChild(2).gameObject;
+		waterLevelUI.SetActive (true);
 
         p1Score = scoreMenuUI.transform.GetChild(0).GetComponent<Image>();
         p2Score = scoreMenuUI.transform.GetChild(1).GetComponent<Image>();
@@ -37,12 +43,18 @@ public class pauseMenu : NetworkBehaviour
 		winnerText = scoreMenuUI.transform.GetChild (3).GetComponent<Text> ();
 		winnerText.text = "";
 
+		MessText = scoreMenuUI.transform.GetChild (4).GetComponent<Text> ();
+		MessText.text = "Total Mess:";
+
         waterLevel = waterLevelUI.transform.GetChild(0).GetComponent<Image>();
         waterLevel.fillAmount = 0;
 
-        p1Score.fillAmount = 0;
-        p2Score.fillAmount = 0;
-        p3Score.fillAmount = 0;
+        p1Score.fillAmount = 1;
+        p2Score.fillAmount = 1;
+        p3Score.fillAmount = 1;
+
+		GameObject[] people = GameObject.FindGameObjectsWithTag ("Player");
+		int numPlayers = people.Length;
 
 		StartCoroutine ( updateTime() );
     }
@@ -87,7 +99,7 @@ public class pauseMenu : NetworkBehaviour
 
     void updateScore()
     {
-        Vector4 scores = SplatManagerSystem.instance.scores + new Vector4(0.1f, 0.1f, 0.1f, 0.1f);
+		Vector4 scores = SplatManagerSystem.instance.scores;// + new Vector4(0.1f, 0.1f, 0.1f, 0.1f);
         float totalScores = scores.x + scores.y + scores.z + scores.w;
         float yellowScore = (scores.x / totalScores);
         float redScore = (scores.y / totalScores);
@@ -95,15 +107,24 @@ public class pauseMenu : NetworkBehaviour
         float blueScore = (scores.w / totalScores);
 
 		Debug.Log ("Total: " + totalScores);
-        //Debug.Log("Press");
-        //Debug.Log("Y" + yellowScore);
-        //Debug.Log("R" + redScore);
-        //Debug.Log("G" + greenScore);
-        //Debug.Log("B" + blueScore);
 
-        p1Score.fillAmount = yellowScore;
-        p2Score.fillAmount = greenScore;
-        p3Score.fillAmount = redScore;
+		if (totalScores > 1.5) 
+			scores = scores / totalScores;
+		
+		if(totalScores > 0.75 * numPlayers){
+			MessText.text = "Total Mess: " + totalScores.ToString("n2") + "! The house has gone to the dogs!";
+		} else {
+			MessText.text = "Total Mess: " + totalScores.ToString("n2") + ". Man rules this roost.";
+		}
+
+		p1Score.rectTransform.sizeDelta = new Vector2(scores.x * Screen.width, p1Score.rectTransform.sizeDelta.y);
+
+		p2Score.rectTransform.localPosition = new Vector3 (p1Score.rectTransform.localPosition.x + p1Score.rectTransform.sizeDelta.x, p2Score.rectTransform.localPosition.y, p2Score.rectTransform.localPosition.z);
+		p2Score.rectTransform.sizeDelta = new Vector2(scores.y * Screen.width, p2Score.rectTransform.sizeDelta.y);
+
+		p3Score.rectTransform.localPosition = new Vector3 (p2Score.rectTransform.localPosition.x + p2Score.rectTransform.sizeDelta.x, p3Score.rectTransform.localPosition.y, p3Score.rectTransform.localPosition.z);
+		p3Score.rectTransform.sizeDelta = new Vector2(scores.z * Screen.width, p3Score.rectTransform.sizeDelta.y);
+
     }
 
     public void updateWaterLevel(float change){
@@ -117,9 +138,6 @@ public class pauseMenu : NetworkBehaviour
 
 		Vector4 scores = SplatManagerSystem.instance.scores + new Vector4(0.1f, 0.1f, 0.1f, 0.1f);
 		float totalScores = scores.x + scores.y + scores.z + scores.w;
-
-		GameObject[] people = GameObject.FindGameObjectsWithTag ("Player");
-		int numPlayers = people.Length;
 
 		if (totalScores < 0.8 * numPlayers)
 			winnerText.text = "THE MAN IS THE WINNER!";
@@ -145,7 +163,7 @@ public class pauseMenu : NetworkBehaviour
 			int time = (int)SplatManagerSystem.instance.gameTimer;
 			int minutes = time / 60;
 			int seconds = time % 60;
-			winnerText.text = minutes.ToString () + ":" + seconds.ToString ();
+			winnerText.text = minutes.ToString () + ":" + seconds.ToString ("D2");
 
 			yield return new WaitForSeconds (1.0f);
 		}
