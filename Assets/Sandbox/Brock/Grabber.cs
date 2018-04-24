cd2280c02d7a17a58de72b14087c52052aa7997f
James Gentry <18251756+palpitatertot@users.noreply.github.com>
2018-04-24 17:15:18 -0500

    Non-shakey holding items

--- Assets/Sandbox/Brock/Grabber.cs
+++ Assets/Sandbox/Brock/Grabber.cs
@@ -6,62 +6,72 @@ using UnityEngine.Networking;
 public class Grabber : NetworkBehaviour {
 
     [SyncVar]
-	public bool hasObject = false;
+    public bool hasObject = false;
 
-	public GameObject handPosition;
+    public GameObject handPosition;
 
-	Grabbable g;
+    Grabbable g;
 
     [SyncVar]
-	private bool inRange;
+    private bool inRange;
 
-	public void OnTriggerEnter(Collider other){
+    public void OnTriggerEnter(Collider other){
         if(isServer){
             inRange = true;    
             g = other.gameObject.transform.parent.gameObject.GetComponent<Grabbable> (); 
         }
-		
-	}
+        
+    }
 
-	public void OnTriggerExit(Collider other){
+    public void OnTriggerExit(Collider other){
         if(isServer){
             inRange = false;
             g = null;    
         }
-		
-	}
-	// Use this for initialization
-	void Start () {
-		
-	}
+        
+    }
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    void _grab(GameObject go){
+        go.transform.parent = gameObject.transform;
+        go.transform.position = handPosition.transform.position;
+        go.GetComponent<Rigidbody>().isKinematic = true;
+        go.GetComponent<NetworkTransform>().transformSyncMode = NetworkTransform.TransformSyncMode.SyncNone;
+        go.transform.GetChild(0).gameObject.GetComponent<Collider>().enabled = false;
+        hasObject = true;
+    }
 
     void Grab(){
         if(isServer){
             if (g == null) { return; }
-            g.gameObject.transform.parent = gameObject.transform;
-            g.gameObject.transform.position = handPosition.transform.position;
-            //g.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            g.gameObject.transform.GetChild(0).gameObject.GetComponent<Collider>().enabled = false;
-            hasObject = true;
+            _grab(g.gameObject);
             RpcGrab(g.gameObject);
         } else{
-            //if (g == null) { return; }
             CmdGrab();
         }
+    }
+
+    void _release(){
+        int heldItemLoc = 3;
+        if (isLocalPlayer)
+        {
+            heldItemLoc = 4;
+        }
+        GameObject go = gameObject.transform.GetChild(heldItemLoc).gameObject;
+        go.GetComponent<NetworkTransform>().transformSyncMode = NetworkTransform.TransformSyncMode.SyncRigidbody3D;
+        go.GetComponent<Rigidbody>().isKinematic = false;
+        go.transform.GetChild(0).GetComponent<Collider>().enabled = true;
+        go.gameObject.transform.SetParent(null);
+        hasObject = false;
     }
 
     void Release(){
         if (isServer)
         {
-            int heldItemLoc = 3;
-            if (isLocalPlayer)
-            {
-                heldItemLoc = 4;
-            }
-            gameObject.transform.GetChild(4).gameObject.GetComponent<Rigidbody>().isKinematic = false;
-            gameObject.transform.GetChild(4).gameObject.transform.GetChild(0).GetComponent<Collider>().enabled = true;
-            gameObject.transform.GetChild(4).gameObject.transform.SetParent(null);
-            hasObject = false;
+            _release();
             RpcRelease();
         }
         else
@@ -74,26 +84,16 @@ public class Grabber : NetworkBehaviour {
     [Command]
     void CmdGrab(){
         Debug.Log("Command:Grabbing");
-        g.gameObject.transform.parent = gameObject.transform;
-        g.gameObject.transform.position = handPosition.transform.position;
-        g.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-        g.gameObject.transform.GetChild(0).gameObject.GetComponent<Collider>().enabled = false;
-        hasObject = true;
+        if (g == null) { return; }
+        GameObject go = g.gameObject;
+        _grab(go);
         RpcGrab(g.gameObject);
     }
 
     [Command]
     void CmdRelease(){
         Debug.Log("Command:Releasing");
-        int heldItemLoc = 3;
-        if (isLocalPlayer)
-        {
-            heldItemLoc = 4;
-        }
-        gameObject.transform.GetChild(heldItemLoc).gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        gameObject.transform.GetChild(heldItemLoc).gameObject.transform.GetChild(0).GetComponent<Collider>().enabled = true;
-        gameObject.transform.GetChild(heldItemLoc).gameObject.transform.SetParent(null);
-        hasObject = false;
+        _release();
         RpcRelease();
     }
 
@@ -104,10 +104,7 @@ public class Grabber : NetworkBehaviour {
         {
             Debug.Log("RPC:Grabbing is server, bailing");
         }
-        go.transform.parent = gameObject.transform;
-        go.transform.position = handPosition.transform.position;
-        //GetComponent<Rigidbody>().isKinematic = true;
-        go.transform.GetChild(0).gameObject.GetComponent<Collider>().enabled = false;
+        _grab(go);
     }
 
     [ClientRpc]
@@ -115,34 +112,29 @@ public class Grabber : NetworkBehaviour {
         Debug.Log("RPC:Releasing");
         if(isServer){
             Debug.Log("RPC:Releasing is server, bailing");
+            return;
         }
-        int heldItemLoc = 3;
-        if(isLocalPlayer){
-            heldItemLoc = 4;
-        }
-        gameObject.transform.GetChild(heldItemLoc).gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        gameObject.transform.GetChild(heldItemLoc).gameObject.transform.GetChild(0).GetComponent<Collider>().enabled = true;
-        gameObject.transform.GetChild(heldItemLoc).gameObject.transform.SetParent(null);
+        _release();
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 
-		if (Input.GetKeyDown (KeyCode.E) && !hasObject && inRange) {
+        if (Input.GetKeyDown (KeyCode.E) && !hasObject && inRange) {
             Grab();
-			
-		} else if (Input.GetKeyDown (KeyCode.E) && hasObject) {
+            
+        } else if (Input.GetKeyDown (KeyCode.E) && hasObject) {
             Release();
-		}
+        }
    
 
 
     }
-	
+    
   
 }
     
-	
+    
     
     
     
