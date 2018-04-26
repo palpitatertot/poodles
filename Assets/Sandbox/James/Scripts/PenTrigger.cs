25dcaab10c2258a1be0afa9c8a46d95d36ead069
CollieSaloon <vcv5733@louisiana.edu>
2018-04-26 09:59:14 -0500

    starting pen trigger

---
+++ Assets/Sandbox/James/Scripts/PenTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //get the dog the man is holding if he is holding a dog
+
+        Grabber grabber = other.gameObject.transform.parent.GetComponent<Grabber>();
+
+        if (g.hasDog)
+        {
+            //SpawnController s = grabber.g.gameObject.transform.parent
+        }
+
+        //get man's grabber
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
